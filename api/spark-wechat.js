23dae01cb16e563433b2a94f96d1bfccfdcdd13f
meet <liuyujian4857@163.com>
2024-12-25 15:24:10 +0800

    更新

--- api/spark-wechat.js
+++ api/spark-wechat.js
@@ -203,6 +203,7 @@ module.exports = async function (request, response) {
   const data = genParams(userChatHistory[FromUserName]);
 
   const connect = await getConnect();
+  // 发送websocket请求
   console.log("会话发送内容：", data);
   connect.send(JSON.stringify(data));
 
